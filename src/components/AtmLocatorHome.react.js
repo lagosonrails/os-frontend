fdaa4b20da376e4c235b9381cad8c01ce59a9af7
Kayode Adeniyi <kayode.adeniyi@andela.co>
2016-12-30 06:20:07 +0100

    Add loader

--- src/components/AtmLocatorHome.react.js
+++ src/components/AtmLocatorHome.react.js
@@ -2,6 +2,7 @@ import React from 'react'
 import GoogleMap from 'google-map-react'
 import MyGreatPlace from './my_great_place'
 import SearchBox from './SearchBox.react'
+import loader from '../assets/ripple.gif'
 import toastr from 'toastr'
 
 import '../styles/atmLocator.styl'
@@ -32,30 +33,36 @@ export default class AtmLocatorHome extends React.Component {
   }
 
   render() {
-    if (!this.state.coordinates.lat)
-      return null
+    // if (!this.state.coordinates.lat)
+    //   return
     return (
       <div className='atm-locator'>
         <div>
           <SearchBox searchByLocationOrBank={this.searchByLocationOrBank} />
-          <h2 className='intro'>Here are the ATMs around you</h2>
+          <h2 className='intro'>{this.state.coordinates.lat ? 'Here are ' : 'Loading '}the ATMs around you</h2>
         </div>
-        <GoogleMap
-          defaultCenter={this.state.coordinates}
-          defaultZoom={this.props.zoom}
-          bootstrapURLKeys={{
-            key: 'AIzaSyCeVyVf5rS9hVsbhdJn3wwBYohwx8WHw_g'
-          }}>
-          <MyGreatPlace {...this.state.coordinates} marker />
-          <MyGreatPlace lat={6.5253793} lng={3.3799057} />
-          <MyGreatPlace lat={6.5244793} lng={3.3796057} />
-          <MyGreatPlace lat={6.5247793} lng={3.3797057} />
-          <MyGreatPlace lat={6.5255793} lng={3.3798057} />
-          <MyGreatPlace lat={6.5245793} lng={3.3858057} />
-          <MyGreatPlace lat={6.5245793} lng={3.3792057} />
-          <MyGreatPlace lat={6.5255793} lng={3.3822057} />
-          <MyGreatPlace lat={6.5545793} lng={3.3892057} />
-        </GoogleMap>
+        {
+          this.state.coordinates.lat ?
+          (
+            <GoogleMap
+              defaultCenter={this.state.coordinates}
+              defaultZoom={this.props.zoom}
+              bootstrapURLKeys={{
+                key: 'AIzaSyCeVyVf5rS9hVsbhdJn3wwBYohwx8WHw_g'
+              }}>
+              <MyGreatPlace {...this.state.coordinates} marker />
+              <MyGreatPlace lat={6.5253793} lng={3.3799057} />
+              <MyGreatPlace lat={6.5244793} lng={3.3796057} />
+              <MyGreatPlace lat={6.5247793} lng={3.3797057} />
+              <MyGreatPlace lat={6.5255793} lng={3.3798057} />
+              <MyGreatPlace lat={6.5245793} lng={3.3858057} />
+              <MyGreatPlace lat={6.5245793} lng={3.3792057} />
+              <MyGreatPlace lat={6.5255793} lng={3.3822057} />
+              <MyGreatPlace lat={6.5545793} lng={3.3892057} />
+            </GoogleMap>
+          ) :
+          <img className='loader' src={loader} />
+        }
       </div>
     )
   }
